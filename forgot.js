27d78eba1b694a26bce88cf423e7126f9b72dd54
Castoxity Sama <sameerrajchal@gmail.com>
2024-04-25 17:35:40 +0545

    final push

---
+++ forgot.js
@@ -0,0 +1,10 @@
+function submitForm(event) {
+    event.preventDefault(); // Prevent form submission
+    var inputField = document.getElementById('email');
+    if (!inputField.value.trim()) {
+        alert('Please fill in the input field.'); // Show an alert or other message
+    } else {
+        window.location.href = 'first.html';
+
+    }
+}
